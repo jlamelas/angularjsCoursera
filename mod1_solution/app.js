c722f71dc7fdec0bca7aca45b3788cedf7b8b770
Juan Lamelas Alfonso <jam.lamelas@gmail.com>
2016-11-14 00:34:41 +0100

    Added controller functionality

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -10,13 +10,26 @@
   function LunchCheckController ($scope) {
     $scope.lunch = "";
     $scope.message = "";
-
+    
     $scope.check = function () {
-      if ($scope.lunch.length <= 3) {
+      if ($scope.lunch === "") {
+        $scope.message = "Please enter data first";
+      } else {
+
+      var lunchList = splitLunch($scope.lunch);
+
+      if (lunchList <= 3) {
         $scope.message = "Enjoy!";
       } else {
-        $scope.message = "It is too much!";
-      }
+        $scope.message = "Too much!";
+      }}
     };
+
+    function splitLunch (string){
+      var comma = ",";
+      var lunchArray = $scope.lunch.split(comma);
+      var lunchItems = lunchArray.length;
+      return lunchItems;
+    }
   };
 })();
