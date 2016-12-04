d42decda2468780090848b46ae03fb5b56e3aef4
Juan Lamelas Alfonso <jam.lamelas@gmail.com>
2016-12-04 23:07:44 +0100

    Narrow it down button implemented

--- mod3_solution/js/app.js
+++ mod3_solution/js/app.js
@@ -1,19 +1,55 @@
 (function () {
+		'use strict';
 		angular
 				.module('NarrowItDownApp', [])
 				.controller('NarrowItDownController', NarrowItDownController)
-				.service('MenuSearchService', MenuSearchService);
+				.service('MenuSearchService', MenuSearchService)
+				.directive('foundItems', FoundItems);
+
+		function FoundItems () {
+				var ddo = {
+						scope: {
+								foundItems: '<'
+						},
+						templateUrl: "snippets/founditems.html"
+				};
+				return ddo;
+		}
 		
 		NarrowItDownController.$inject = ['MenuSearchService'];
 		function NarrowItDownController (MenuSearchService) {
 				var controller = this;
+			
+				controller.showFound = function (searchTerm) {
+						MenuSearchService.getMatchedMenuItems(searchTerm)
+								.then(function (response) {
+										controller.found = response;
+								});
+				};
 		}
 
 		MenuSearchService.$inject = ['$http'];
 		function MenuSearchService ($http) {
 				var service = this;
+				
 				service.getMatchedMenuItems = function (searchTerm) {
-						
+						var response = $http({
+								method: "GET",
+								url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+						});
+						return response
+								.then(function (response) {
+										var items = response.data.menu_items;
+										var foundItems = [];
+										for (var i = 0; i < items.length; i++) {
+												if (items[i].description.includes(searchTerm)) {
+														foundItems.push(items[i]);
+												} else {
+														console.log("Nothing found");
+												}
+										}
+										return foundItems;
+								});
 				};
 		}
 		
