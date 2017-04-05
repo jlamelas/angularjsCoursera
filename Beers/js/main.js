efe917a7b03e09ad81af637479b73e0ca2036f00
Juan Lamelas Alfonso <jam.lamelas@gmail.com>
2017-04-05 19:22:53 +0200

    Search implemented

--- Beers/js/main.js
+++ Beers/js/main.js
@@ -1,17 +1,25 @@
 vm = new Vue({
     el: '#app',
     data: {
-        beers: []
+        beers: [],
+        searchTerm: ''
     },
     methods: {
-        getBeers () {
-            axios.get('https://api.punkapi.com/v2/beers/')
-                .then((response) => {
-                    this.beers = response.data;
-                });
+        getSearch () {
+            axios.get('https://api.punkapi.com/v2/beers').then(response => {
+                this.searchTerm = this.searchTerm.trim();
+                this.beers = [];
+                for (var i = 0; i < response.data.length; i++) {
+                    if (this.searchTerm === '') {
+                        return this.beers = [];
+                        break;
+                    } else if (response.data[i].description.includes(this.searchTerm) || response.data[i].name.includes(this.searchTerm)) {
+                        this.beers.push(response.data[i]);
+                    }
+                }
+                this.searchTerm = '';
+                return this.beers;
+            });
         }
-    },
-    mounted () {
-        this.getBeers();
     }
 });
